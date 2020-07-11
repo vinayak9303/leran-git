430c4c91097d0120ac334c6ff11b2570770e7528
tejasvita nale <tejasvianale@gmail.com>
2020-07-11 17:37:38 +0530

    msg

---
+++ index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NEWSTARTUP</title>
+<head>
+<body>
+	<h1 align="center" style="color: blue">welcome on my page<h1>
+		<p>
+			<h3>Hi..</h3>
+			<h4 style="color: pink "> I am trying to do my best in web development leraning. </h4>
+		</p>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QCTtc36u-Kk?start=39" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       <p>
+       	<img align="right" sizes="height=300px width=300px" src="E:\WhatsApp Profile Photos\+91 83083 34748 20191118_090930.jpg">
+       </p> 
+</body>	
+</html>
